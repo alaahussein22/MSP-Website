19063a8db319359609980622a91804cb4ad511f7
alaahussein22 <56509626+alaahussein22@users.noreply.github.com>
2020-07-12 01:56:48 +0200

    Create SectionImage.js
    this is for image

---
+++ src/components/home/AboutSection/SectionImage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SectionImage.css'
+
+
+
+const SectionImage = () => {
+
+   return (
+  <div className="col-lg-6 col-md-6 col-sm-12">
+    
+  </div>
+
+   )
+}
+export default SectionImage;
