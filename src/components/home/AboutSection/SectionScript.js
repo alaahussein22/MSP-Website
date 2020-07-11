e9a1f86a7e732021371e664c1fde1d3c765a2f0b
alaahussein22 <56509626+alaahussein22@users.noreply.github.com>
2020-07-12 01:54:00 +0200

    Create SectionScript.js
    this is about Section

---
+++ src/components/home/AboutSection/SectionScript.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './SectionScript.css'
+
+
+const SectionScript = () => {
+
+   return (
+
+    <div className="script">
+        <div className="row">
+
+
+          <p>
+
+          MSP Tech Club at Al-Azher is a student community program that promotes <br />
+          advanced technology through education , practice and innovation . It also <br />
+           privides students with both Technical and non-technical sessions needed <br />
+          which is packing their lives with high level of skills and supportings their <br />
+          careers with opportunities.<br />
+
+          Our Mission : MSP Tech club at Al-Azher has a clear mission to help the <br />
+           students in the camps and to be there for any kind of support needed <br />
+            whether it's technical or non- technical and to help them find their most <br />
+            suitable career.<br />
+
+          Our products : Technical sessions , soft skills , workshops , online courses <br />
+           and Compatitions . <br />
+
+          </p>
+             
+            <div className="col-lg-6 col-md-6 col-sm-12">
+             <button>More</button>
+            </div>
+   </div>
+    </div>
+
+   );
+
+
+}
+export default SectionScript
