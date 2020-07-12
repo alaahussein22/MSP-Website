1fa10ea204a35613245ba14c78bf13a02f48a1d9
alaahussein22 <56509626+alaahussein22@users.noreply.github.com>
2020-07-12 02:01:41 +0200

    Update App.js
    this is about Section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,30 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+  
+  <div className="App">
+    
+    //start Section
+
+      <div className="container">
+        <div className="row">
+     
+            <img src= {Image}  />
+     
+             <SectionScript />
+     </div>
+
+      <SectionImage />
+
+    </div>
+
+  
+
+/* End Section*/
+
+    
+    </div>;
 }
 
 export default App;
